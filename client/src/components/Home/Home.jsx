59c8512e2f372dc09fdc5bdc08038c8173fd987c
Valentin <valentinvalentinov027@gmail.com>
2023-11-03 21:56:07 +0200

    Add image to home page and card

--- client/src/components/Home/Home.jsx
+++ client/src/components/Home/Home.jsx
@@ -1,5 +1,32 @@
+import styles from './Home.module.css';
+
+import Card from 'react-bootstrap/Card';
+
 const Home = () => {
-    return <h1 className="text-center">Hello from PlatePal!</h1>
+    return (
+        <>
+            <section className={`${styles.homeSection}`}>
+                <Card className={styles.welcomeCard}>
+                    <Card.Header className={styles.welcomeCardHeader}>PlatePal</Card.Header>
+                    <Card.Body>
+                        <Card.Title>Your Culinary Companion</Card.Title>
+                        <Card.Text>
+                            Discover a world of culinary delights with PlatePal. Whether you're a seasoned chef or a kitchen novice, our platform has something for everyone. Browse a vast collection of mouthwatering recipes, from classic favorites to exotic dishes.
+
+                            Join our passionate community of food enthusiasts, where you can share your own culinary creations and connect with fellow foodies. Explore user-generated content, leave comments, and get inspired to try new recipes.
+
+                            PlatePal is your one-stop destination for all things food. Start your gastronomic journey today and turn your kitchen into a culinary paradise. Bon appétit!
+                        </Card.Text>
+                        {/* <Button variant="primary">Go somewhere</Button> */}
+                    </Card.Body>
+                </Card>
+
+            </section>
+            {/* <div>
+                <p>fdsfsd</p>
+            </div> */}
+        </>
+    );
 };
 
 export default Home;
