1702abaae0e39de9475b8f50ee5a17e29a855438
Valentin <valentinvalentinov027@gmail.com>
2023-11-05 19:43:32 +0200

    Show retrieved recipes on the client

--- client/src/components/Recipes/Recipes.jsx
+++ client/src/components/Recipes/Recipes.jsx
@@ -1,8 +1,35 @@
+import { useEffect, useState } from 'react';
 import { useParams } from 'react-router-dom';
+
+import Card from 'react-bootstrap/Card';
+
+import styles from './Recipes.module.css';
 
 const Recipes = () => {
     const { category } = useParams();
-    return <h2>All Recipes {category}!</h2>;
+    const [recipes, setRecipes] = useState([]);
+
+    useEffect(() => {
+        fetch(`http://localhost:3000/recipe/all/${category}`)
+            .then(res => res.json())
+            .then(res => setRecipes(res.result))
+            .catch(error => console.log(error));
+    }, []);
+
+    return (
+        <>
+            <div className={styles.container}>
+                {recipes.map(x => (
+                    <Card key={x._id} className={styles.recipeCard}>
+                        <Card.Img variant="top" src={x.image.url} />
+                        <Card.Body className={styles.recipeCardBody}>
+                            <Card.Title className={styles.recipeCardTitle}>{x.name}</Card.Title>
+                        </Card.Body>
+                    </Card>
+                ))}
+            </div>
+        </>
+    );
 };
 
 export default Recipes;
