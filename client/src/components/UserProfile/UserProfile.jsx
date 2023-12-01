5ff0856cbd4963069c045bb16ab6c0f41a0f176c
Valentin <valentinvalentinov027@gmail.com>
2023-12-01 16:39:45 +0200

    Update UserProfile component

--- client/src/components/UserProfile/UserProfile.jsx
+++ client/src/components/UserProfile/UserProfile.jsx
@@ -6,60 +6,77 @@ import Button from 'react-bootstrap/Button';
 import { useContext } from 'react';
 import { Link } from 'react-router-dom';
 
-import { userRecipesCardText } from '../../constants/cardTextMessages';
-
 import { AuthContext } from '../../contexts/AuthContext';
+
+import {
+    manageUsersCardText,
+    favoriteRecipesCardText,
+    userRecipesCardText
+} from '../../constants/cardTextMessages';
+
+import BackToTopArrow from '../BackToTopArrow/BackToTopArrow';
 
 const UserProfile = () => {
     const { isAdmin, username } = useContext(AuthContext);
 
     return (
-        <div className={styles.container}>
-            <h2>My Profile</h2>
-            <h3>Username: {username}</h3>
+        <>
+            <h2 className='text-center mt-3'>My Profile</h2>
+            <h3 className='text-center'>Username: {username}</h3>
+
             {isAdmin && (
-                <Card className={styles.userRecipesCard}>
+                <div className={styles.container}>
+                    <img src='/src/assets/images/manageUsers.jpg' alt="" />
+                    <Card className={styles.card}>
+                        <Card.Body>
+                            <Card.Text>
+                                {manageUsersCardText}
+                            </Card.Text>
+                            <Link to='/manage-users'>
+                                <Button bsPrefix={styles.cardBtn}>
+                                    Go To Manage Users
+                                </Button>
+                            </Link>
+                        </Card.Body>
+                    </Card>
+                </div>
+            )}
+
+            <div className={styles.container}>
+                <img src='/src/assets/images/myRecipes.jpg' alt="" />
+                <Card className={styles.card}>
                     <Card.Body>
                         <Card.Text>
-                            Manage Users
+                            {userRecipesCardText}
                         </Card.Text>
-                        <Link to='/manage-users'>
-                            <Button bsPrefix={styles.userRecipesCardBtn}>
-                                Go To Manage Users
+                        <Link to='/recipes/user-recipes/all'>
+                            <Button bsPrefix={styles.cardBtn}>
+                                Go To My Recipes
                             </Button>
                         </Link>
                     </Card.Body>
                 </Card>
-            )}
+            </div>
 
+            <div className={styles.container}>
+                <img src='/src/assets/images/favoriteRecipes-min.jpg' alt="" />
+                <Card className={styles.card}>
+                    <Card.Body>
+                        <Card.Text>
+                            {favoriteRecipesCardText}
+                        </Card.Text>
+                        <Link to='/recipes/user-favourites'>
+                            <Button bsPrefix={styles.cardBtn}>
+                                Go To My Favourite Recipes
+                            </Button>
+                        </Link>
 
-            <Card className={styles.userRecipesCard}>
-                <Card.Body>
-                    <Card.Text>
-                        {userRecipesCardText}
-                    </Card.Text>
-                    <Link to='/recipes/user-recipes/all'>
-                        <Button bsPrefix={styles.userRecipesCardBtn}>
-                            Go To My Recipes
-                        </Button>
-                    </Link>
-                </Card.Body>
-            </Card>
+                    </Card.Body>
+                </Card>
+            </div>
 
-            <Card className={styles.userRecipesCard}>
-                <Card.Body>
-                    <Card.Text>
-                        Welcome to your culinary haven! Explore the delightful world of flavors by diving into your curated collection of favorite recipes. This personalized list is a testament to your culinary journey, showcasing the dishes that have won a special place in your heart and kitchen. Click the link below to savor the memories and discover the magic behind each recipe. Happy cooking!
-                    </Card.Text>
-                    <Link to='/recipes/user-favourites'>
-                        <Button bsPrefix={styles.userRecipesCardBtn}>
-                            Go To My Favourite Recipes
-                        </Button>
-                    </Link>
-
-                </Card.Body>
-            </Card>
-        </div>
+            <BackToTopArrow />
+        </>
     )
 };
 
